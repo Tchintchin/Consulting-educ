5f2b5b603ec5216fc39ff525277d8d2730c4673c
Voltaire <voltaireoscar@gmail.com>
2024-05-16 16:00:13 +0000

    first commit

---
+++ resources/views/extend/header.blade.php
@@ -0,0 +1,165 @@
+<!-- Top Bar -->
+    <div class="site-top-bar">
+
+        <!-- Bootstrap -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+
+                    <!-- Left section -->
+                    <div class="site-top-bar-left-section">
+
+                        <!-- Link -->
+                        <a>
+                            <!-- Font awesome icon -->
+                            <i class="fa fa-clock-o"></i>
+                            <!-- Text -->
+                            <span>Du Lundi au Vendredi : 08:00 - 18:00</span>
+                        </a>
+
+                    </div>
+                    <!-- End Left Section -->
+
+
+                    <!-- Right Section -->
+                    <div class="site-top-bar-right-section">
+
+                        <!-- Link -->
+                        <a href="tel:+1801454587">
+                            <!-- Icon -->
+                            <i class="fa fa-phone"></i>
+                            <!-- Text -->
+                            <span>+225 0759 9236 05 - 0779 5113 83</span>
+                            <span>/ +228 0759 9236 05 - 0779 5113 83</span>
+                        </a>
+
+                        <!-- Link -->
+                        <a href="mailto:contact@gmail.com">
+                            <!-- Icon -->
+                            <i class="fa fa-envelope"></i>
+                            <!-- Text -->
+                            <span>infos@ibm-t.net</span>
+                        </a>
+
+                    </div>
+                    <!-- End Right Section -->
+
+                </div>
+            </div>
+        </div>
+        <!-- End Bootstrap -->
+
+    </div>
+    <!-- End Top Bar -->
+
+    <!-- Header -->
+    <header class="site-header" id="sticky-header">
+
+        <!-- Bootstrap -->
+        <div class="container">
+            <div class="row">
+
+            <div class="col-xs-2">
+<!-- Logo -->
+<div class="site-logo">
+                        <!-- Link -->
+                        <a href="index.html">
+                            <!-- Logo Image -->
+                            <img src="img/ibmtcons1.png" alt="Logo">
+                        </a>
+                    </div>
+                    <!-- End logo -->
+            </div>
+
+            
+                <div class="col-xs-10">
+
+                    
+
+                    <!-- Navigation Toggle Button -->
+                    <div class="site-nav-toggle">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <!-- End Nav Toggle Button -->
+
+                    <!-- Navigation -->
+                    <nav class="site-nav">
+                        <ul>
+                            <!-- Active Item (Use the active class) -->
+                            <li class="active"><a href="index.html">ACCEUIL</a></li>
+
+                           <!--   <li><a href="#">Notre Metier
+                                <i class="indicator-desktop fa fa-angle-down"></i> </a> 
+                                <i class="indicator-mobile fa fa-angle-down"></i>      
+
+                                <ul class="sub-menu">
+                                    <li><a href="work-1.html">work-1</a></li>
+                                    <li><a href="work-2.html">work-2</a></li>
+                                    <li><a href="work-3.html">work-3</a></li>
+                                </ul>
+                            </li>-->
+
+                            <li><a href="work-1.html">QUI SOMMES NOUS</a></li>
+
+                            <!-- <li><a href="about-us.html">About us</a></li> -->
+
+                            <li><a href="#">NOS PRESTATIONS 
+                                <i class="indicator-desktop fa fa-angle-down"></i> </a> <!-- Desktop sub menu indicator icon -->
+                                <i class="indicator-mobile fa fa-angle-down"></i>       <!-- Mobile view sub menu indicator icon -->
+
+                                <!-- Sub menu level 1 -->
+                                <ul class="sub-menu">
+                                    <li><a href="services-1.html">Enseignement supérieur</a></li>
+                                    <li><a href="services-2.html">Enseignement Secondaire </a></li>
+                                    <li><a href="services-3.html">Enseignement primaire </a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="#">NOS CIBLES 
+                                <i class="indicator-desktop fa fa-angle-down"></i> </a> <!-- Desktop sub menu indicator icon -->
+                                <i class="indicator-mobile fa fa-angle-down"></i>       <!-- Mobile view sub menu indicator icon -->
+
+                                <!-- Sub menu level 1 -->
+                                <ul class="sub-menu">
+                                    <li><a href="services-1.html">Particuliers (Parents d’élèves, Etudiants, Travailleurs) </a></li>
+                                    <li><a href="services-2.html">Organisations (Etats, Universités, Entreprises, 
+                                        Associations professionnelles, Institutions non-gouvernementales.) </a></li>
+
+                                </ul>
+                            </li>
+                            
+                            <li><a href="contact-us.html">NOUS CONTACTER</a></li>
+                           <!-- <li><a href="testimonials.html">Carrières</a></li>-->
+                            
+                            <li><a href="#">AUTRES RUBRIQUES
+                                <i class="indicator-desktop fa fa-angle-down"></i> </a> <!-- Desktop sub menu indicator icon -->
+                                <i class="indicator-mobile fa fa-angle-down"></i>       <!-- Mobile view sub menu indicator icon -->
+
+                                <!-- Sub menu level 1 -->
+                                <ul class="sub-menu">
+                                <li><a href="services-details.html">Newsletters</a></li>
+                                    <li><a href="services-details.html">Prendre RDV en ligne </a></li>
+                                    <li><a href="case-details.html">FAQ</a></li>
+                                    <li><a href="case-details.html">Politique de confidentialité</a></li>
+                                    <li><a href="case-details.html">Preférence en matière de cookies</a></li>
+                                    <li><a href="case-details.html">Conditions d'utilisation</a></li>
+                                    <li><a href="case-details.html">Nos références </a></li>
+                                    <li><a href="case-details.html">Paiement en ligne</a></li>
+                                    <li><a href="case-details.html">Contacts</a></li>
+                                </ul>
+                            </li>
+                            
+                            
+                        </ul>
+                    </nav>
+                    <!-- End Navigation -->
+
+                </div>
+            </div>
+        </div>
+        <!-- End Bootstrap -->
+
+    </header>
+    <!-- End Header -->
---
+++ resources/views/extend/link.blade.php
@@ -0,0 +1,15 @@
+ <!-- Necessary CSS Files -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">            									<!-- Bootstrap CSS Only GRID -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/google-font.css">              									<!-- Google Font -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">         									<!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="css/swiper.min.css">               									<!-- Swiper slider -->
+    <link rel="stylesheet" href="css/jquery.mb.YTPlayer.min.css">   									<!-- Video background -->
+    <link rel="stylesheet" href="css/template.css">                 									<!-- Template CSS -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
